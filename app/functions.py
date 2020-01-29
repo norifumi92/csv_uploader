36d7950c275689732f1e7cb5e83b56dd2cf4accd
LAPTOP-TERIQ5LU\norif <noristyle92@gmail.com>
2020-01-29 13:21:54 +0900

    Implemented csv uload and download functions

--- app/functions.py
+++ app/functions.py
@@ -1,8 +1,13 @@
+import csv
+from django.http import HttpResponse  
+
+### Upload files under app/static/upload
 def upload_file(f):  
     with open('app/static/upload/'+f.name, 'wb+') as destination:  
         for chunk in f.chunks():  
             destination.write(chunk)
 
+### Process csv file
 def process_csv_file(f):  
     file_data = f.read().decode("utf-8")
     lines = file_data.split("\r\n")
@@ -15,3 +20,71 @@ def process_csv_file(f):
         total += value
     
     return total
+
+### Process csv files
+def process_files(availability, reservation):  
+    
+        """ Description
+        :type availability:
+        :param availability:
+    
+        :type reservation:
+        :param reservation:
+    
+        :raises:
+    
+        :rtype:
+        """
+        availability_dict = convert_to_dict(availability)
+        reservation_dict = convert_to_dict(reservation)
+    
+        csv_data = []
+
+        courses = list(availability_dict)
+    
+        for course in courses:
+            remaining = availability_dict[course] - reservation_dict[course]
+            row = [course, remaining]
+            csv_data.append(row)
+        return csv_data
+
+def convert_to_dict(file):
+    
+        """ Description
+        :type file:
+        :param file:
+    
+        :raises:
+    
+        :rtype:
+        """
+        data = file.read().decode("utf-8-sig")
+        lines = data.split("\r\n")
+        dict = {}
+        for line in lines:	
+            fields = line.split(",")
+            course = fields[0]
+            capacity = int(fields[1])
+            dict[course] = capacity
+
+        return dict
+
+
+def write_into_csv(csv_data):
+        """ Description
+        :type csv_data:
+        :param csv_data:
+    
+        :raises:
+    
+        :rtype:
+        """
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="download.csv"'
+        writer = csv.writer(response)  
+
+        for row in csv_data:
+            writer.writerow(row)
+
+        return response
+
--- app/views.py
+++ app/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render  
 from django.http import HttpResponse  
-from app.functions import upload_file
-#from app.functions import process_csv_file  
+from app.functions import process_files
+from app.functions import write_into_csv  
 from app.forms import ClassroomForm  
 from django.template import loader
-import csv
 
 def index(request):  
     if request.method == 'POST':  
         classroom = ClassroomForm(request.POST, request.FILES)  
-        print(request.FILES)
         if classroom.is_valid():  
-            upload_file(request.FILES['availablity'])
-            upload_file(request.FILES['reservation'])
+            availability = request.FILES['availablity']
+            reservation = request.FILES['reservation']
+            #handle_uploaded_file(request.FILES['file']) 
+            #total = process_csv_file(request.FILES['file'])
+            #csv_data = [
+            #    'Total', total
+            #]
+            
+            #implement process_files
+            csv_data = process_files(availability, reservation)
 
-            return HttpResponse("File uploaded successfuly")
-    else:       
+            #download result as a csv format             
+            response = write_into_csv(csv_data)
+            
+            return response
+    else:  
         ###Post ClassroomForm
         classroom = ClassroomForm()  
         return render(request,"index.html",{'form':classroom})  
